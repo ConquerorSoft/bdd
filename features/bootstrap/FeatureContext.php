160e1808919350262a3c7651904198c548fa2895
Christian Varela <dobleclick.mx@gmail.com>
2020-01-20 21:29:18 -0500

    After appending snippets

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
@@ -18,5 +19,53 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given that a plain :arg1 string is provided
+     */
+    public function thatAPlainStringIsProvided($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When the user requires to encode it
+     */
+    public function theUserRequiresToEncodeIt()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the encoded string will be: :arg1
+     */
+    public function theEncodedStringWillBe($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given that an encoded :arg1 string is provided
+     */
+    public function thatAnEncodedStringIsProvided($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When the user requires to decode it
+     */
+    public function theUserRequiresToDecodeIt()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the plain string will be: :arg1
+     */
+    public function thePlainStringWillBe($arg1)
+    {
+        throw new PendingException();
     }
 }
